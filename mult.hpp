6f1d58e22ace06c855919951f7012efd527cd285
tannnguyen13 <tann2000@gmail.com>
2020-11-02 11:24:04 -0800

    Created the mult.hpp file.

---
+++ mult.hpp
@@ -0,0 +1,18 @@
+#ifndef __MULT_HPP__
+#define __MULT_HPP__
+
+#include "base.hpp"
+#include <string>
+
+Class Mult : public Base {
+	Public :
+		Mult(Base* base1, Base* base2) : Base() { op1 = base1; op2 = base2; }
+		virtual double evaluate() { return ((op1->evaulate()) * (op2->evaluate()));}
+		virtual std::string stringify() { return (op1->stringify() + " * " + op2->stringify());}
+	Private:
+		Base* op1;
+		Base* op2;
+}
+
+#endif //__MULT_HPP__
+
