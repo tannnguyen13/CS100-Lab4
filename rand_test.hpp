e54e95ea66a29c0864568ac8e1bb37a62d530dd2
rrchandler <ryanchandler50@gmail.com>
2020-11-02 11:08:14 -0800

    Add Rand and Op unit tests

---
+++ rand_test.hpp
@@ -0,0 +1,17 @@
+#ifndef __RAND_TEST_HPP__
+#define __RAND_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "rand.hpp"
+
+TEST(RandTest, RandEvaluateNonZero) {
+    Rand* test = new Rand();
+    EXPECT_TRUE((test->evaluate() >= 0) && (test->evaluate() <= 99));
+}
+TEST(RandTest, RandStringify){
+    Rand* test = new Rand();
+    EXPECT_EQ(test->stringify(), std::to_string(test->evaluate()));
+}
+
+#endif //__OP_TEST_HPP__
