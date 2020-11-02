f35eeee4ed0ec1f1363a46504770fc5ac2155e27
tannnguyen13 <tann2000@gmail.com>
2020-11-02 12:08:32 -0800

    Fixed the problem with the quotation marks.

--- div.hpp
+++ div.hpp
@@ -5,11 +5,11 @@
 #include <string>
 
 class Div : public Base {
-	Public :
+	public :
 		Div(Base* base1, Base* base2) : Base() { op1 = base1; op2 = base2; }
 		virtual double evaluate() { return ((op1->evaluate()) / (op2->evaluate()));}
-		virtual std::string stringify() { return (op1->stringify() + “ / “ + op2->stringify());}
-	Private:
+		virtual std::string stringify() { return (op1->stringify() + " / " + op2->stringify());}
+	private:
 		Base* op1;
 		Base* op2;
 }
