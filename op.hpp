57cbf7fd4ce1e1a1c68345cb7323c1a0888292b9
tannnguyen13 <tann2000@gmail.com>
2020-11-02 10:30:58 -0800

    Edited the op.hpp file to evaluate and stringify and made the CMakeLists.txt file work correctly

--- op.hpp
+++ op.hpp
@@ -2,12 +2,15 @@
 #define __OP_HPP__
 
 #include "base.hpp"
+#include <string>
 
 class Op : public Base {
     public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        Op(double value) : Base() { opVal = value;}
+        virtual double evaluate() { return opVal; }
+        virtual std::string stringify() { return std::to_string(opVal); }
+   private:
+	double opVal;
 };
 
 #endif //__OP_HPP__
